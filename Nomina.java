cf87bbbea611d0e2fd2e3f13d1193686ccecc7bc
OctaG <millennium935@icloud.com>
2017-11-15 11:15:55 -0600

    Separe ent metodos el calculo de las percepciones

--- Nomina.java
+++ Nomina.java
@@ -15,25 +15,47 @@ public class Nomina //Inicio clase Nomina
 {
   public static void main(String[] args) throws IOException
   {
-    double percepciones, deducciones, sueldoNeto;
+    double salarioMensual, salarioDiario, bonos, sueldo, sueldoDiasFeriados, sueldoHorasExtra, asignaciones, percepciones, deducciones, sueldoNeto;
+    int diasTrabajados, horasExtra, feriados;
     String nombreArchivo = "Informacion_Empleados.csv";
     String[][] nomina = leerArchivo(nombreArchivo);
+    Scanner lectura = new Scanner(System.in);
+    int posicionEmpleado;
     String[] empleado = new String[6];
 
     System.out.println("Numero de Nomina:       Nombre:");
     for(int y = 0; y < nomina.length; y++){
       System.out.println(imprimirEmpleado(nomina, y));
     }
+    leerArchivo(nombreArchivo);
+    guardarNomina(nombreArchivo, nomina);
 
-    percepciones = calcularPercepciones(nomina, encontrarEmpleado(nomina));
+    System.out.println("ESCRIBE LA NOMINA DEL EMPLEADO QUE DESEAS BUSCAR");
+    posicionEmpleado = encontrarEmpleado(nomina);
+    salarioMensual = Double.parseDouble(nomina[posicionEmpleado][3]);
+
+    System.out.println("ESCRIBE LA INFORMACION QUE SE PIDE");
+
+    System.out.println("Días Trabajados: ");
+    diasTrabajados = lectura.nextInt();
+
+    System.out.println("Bonos: ");
+    bonos = lectura.nextDouble();
+
+    System.out.println("Días Feriados: ");
+    feriados = lectura.nextInt();
+
+    System.out.println("Horas Extra: ");
+    horasExtra = lectura.nextInt();
+
+    salarioDiario = calcularSalarioDiario(salarioMensual);
+    sueldo = calcularSueldo(salarioDiario, diasTrabajados);
+    sueldoDiasFeriados = calcularSueldoDiasFeriados(salarioDiario, feriados);
+    sueldoHorasExtra = calcularSueldoHorasExtra(salarioDiario, horasExtra);
+    asignaciones = calcularAsignaciones(sueldoDiasFeriados, sueldoHorasExtra, bonos);
+    percepciones = calcularPercepciones(sueldo, asignaciones);
+
     System.out.println(percepciones);
-
-    deducciones = calcularDeducciones(percepciones);
-    System.out.println(deducciones);
-
-    sueldoNeto = calcularSueldoNeto(percepciones, deducciones);
-    System.out.println(sueldoNeto);
-
   }
 
   //leerArchivo guarda los datos de Excel en una matriz
@@ -78,59 +100,62 @@ public class Nomina //Inicio clase Nomina
     }
     escritor.close();
   }
-  //salario saca el total y desglose del salario del empleado
-  public static void salario(String[][] nomina){
-  }
 
   //Este metodo busca al empleado en el arreglo nomina y regresa la posicion en la se encuentra.
   public static int encontrarEmpleado(String[][] nomina){
     Scanner lectura = new Scanner(System.in);
     String nominaEmpleado;
-    int posicionEmpleado = 0;
+    int posicionEmpleado = -1;
 
     System.out.print("Ingrese el número de nómina del trabajador: ");
     nominaEmpleado = lectura.next();
 
     for (int y = 0; y < nomina.length; y++) {
-      if (nomina[y][5].equals(nominaEmpleado)) {
+      if (nomina[y][5].equals(nominaEmpleado)){
           posicionEmpleado = y;
+          break;
       }
     }
     return (posicionEmpleado);
   }
 
-  public static double calcularPercepciones(String[][] nomina, int posicionEmpleado){
-    Scanner lectura = new Scanner(System.in);
-    double percepciones = 0, sueldo = 0, asignaciones = 0, salarioDiario = 0, bonos = 0, diasTrabajados = 0, feriados = 0, horasExtra = 0;
-
-    System.out.println("Escribe la información que se pide: ");
-
-    System.out.println("Dias Trabajados: ");
-    diasTrabajados = lectura.nextInt();
-
-    System.out.println("Bonos: ");
-    bonos = lectura.nextInt();
-
-    System.out.println("Feriados: ");
-    feriados = lectura.nextInt();
-
-    System.out.println("Horas Extra: ");
-    horasExtra = lectura.nextInt();
-
-    salarioDiario = Double.parseDouble(nomina[posicionEmpleado][3]) / 30;
-
-    System.out.println(nomina[posicionEmpleado][3]);
-    sueldo = salarioDiario * diasTrabajados;
-
-    feriados = salarioDiario * feriados;
-    horasExtra = (salarioDiario / 8) * horasExtra;
-
-    asignaciones = feriados + horasExtra + bonos;
+  public static double calcularPercepciones(double sueldo, double asignaciones){
+    double percepciones;
     percepciones = sueldo + asignaciones;
-
     return (percepciones);
   }
 
+  public static double calcularSalarioDiario(double salarioMensual){
+    double salarioDiario;
+    salarioDiario = salarioMensual / 30;
+    return salarioDiario;
+  }
+
+  public static double calcularSueldo(double salarioDiario, int diasTrabajados){
+    double sueldo;
+    sueldo = salarioDiario * diasTrabajados;
+    return (sueldo);
+  }
+
+  public static double calcularSueldoDiasFeriados(double salarioDiario, int feriados){
+    double sueldoDiasFeriados;
+    sueldoDiasFeriados = salarioDiario * (double)feriados;
+    return (sueldoDiasFeriados);
+  }
+
+  public static double calcularSueldoHorasExtra(double salarioDiario, int horasExtra){
+    double sueldoHorasExtra;
+    sueldoHorasExtra = (salarioDiario / 8) * horasExtra;
+    return (sueldoHorasExtra);
+  }
+
+  public static double calcularAsignaciones(double salarioDiasFeriados, double salarioHorasExtra, double bonos){
+    double totalDeAsignaciones;
+    totalDeAsignaciones = salarioDiasFeriados + salarioHorasExtra + bonos;
+    return (totalDeAsignaciones);
+  }
+
+  //Este metodo regresa las deducciones del empleado
   public static double calcularDeducciones(double percepciones){
     Scanner lectura = new Scanner(System.in);
     double deducciones, iva, isr, prestamos;
@@ -146,12 +171,14 @@ public class Nomina //Inicio clase Nomina
     return (deducciones);
   }
 
+  //Este metodo resta las deducciones a las percepciones del empleado
   public static double calcularSueldoNeto(double percepciones, double deducciones){
     double sueldoNeto;
     sueldoNeto = percepciones - deducciones;
     return (sueldoNeto);
   }
 
+  //Este metodo regres el nombre, el apellido y el numero de nomina del empleado en un string
   public static String imprimirEmpleado(String[][] nomina, int y){
     String nombre, apellido, numeroDeNomina;
     nombre = nomina[y][0];
@@ -159,5 +186,7 @@ public class Nomina //Inicio clase Nomina
     numeroDeNomina = nomina[y][5];
     return ("       " +numeroDeNomina + "                " + nombre + " " + apellido);
   }
+
+
 
 }
